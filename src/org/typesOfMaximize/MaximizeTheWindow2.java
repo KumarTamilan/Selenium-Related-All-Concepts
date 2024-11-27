7a836a038e604407c636407dd0d2f049293f5ac3
KumarTamilan <kumaranharsh123@gmail.com>
2024-11-27 12:56:39 +0530

    Selenium Related All Concepts will be added

---
+++ src/org/typesOfMaximize/MaximizeTheWindow2.java
@@ -0,0 +1,15 @@
+package org.typesOfMaximize;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class MaximizeTheWindow2 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",".\\ChromeDriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in/");
+	}
+}
